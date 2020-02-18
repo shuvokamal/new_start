85bfd3dae53a227029024e47af6ec037b4287997
shuvokamal <59034055+shuvokamal@users.noreply.github.com>
2020-02-18 16:31:03 -0500

    formatted

--- initpractice.py
+++ initpractice.py
@@ -11,5 +11,5 @@ class Rectangle:
 r = Rectangle(length=int(input('Enter a length')),breadth= int(input('Enter a breadth')),unit_cost=int(input('Enter unit cost')))
 
 #r = Rectangle(10,20,2000)
-print("Area of Rectangle: %s sq units" + str(r.get_area()))
-print("Cost of rectangular field: Rs%" + str(r.calculate_cost()))
+print("Area of Rectangle: %s sq units"  %(str(r.get_area())))
+print("Cost of rectangular field: Rs%s" %(str(r.calculate_cost())))
